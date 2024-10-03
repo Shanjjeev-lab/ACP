f3ed11d1ab92f6059c2c00a2b621a357affb4aec
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2024-10-03 19:23:51 +0530

    Python Robot Project

---
+++ main.py
@@ -0,0 +1,10 @@
+class Robot:
+    name = "Ron"
+    greet = "And I am welcoming you to the WORLD OF THE FUTURE"
+
+    def display(self):
+        print("Hello, my name is", self.name)
+        print(self.greet)
+
+robot1 = Robot()
+robot1.display()
